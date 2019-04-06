384c50ed8e0f7f8a973c47f0dbaefffab6895c0c
Chris Jacobs <rake74@gmail.com>
2019-04-06 19:10:10 -0400

    saving rough state, get_temperature_from_source mostly implmented

--- python/pdx-temp.py
+++ python/pdx-temp.py
@@ -31,6 +31,11 @@ even if running inside a vagrant box itself running inside a vagrant instance.
 * Use library Requests to obtain temperature from configured source
 * Use library PySQLite to cache temperature
 * Use library time to get current epoch time
+* Use library requests to obtain temperature data
+* Use library json to return, and obtain temperature data
+* openweathermap api key is required as first argument
+  There's certainly better ways to do this, or managing an api key.
+  As no data source nor api key were provided, I signed up for one.
 
 # Further notes:
 I did not come up with all the code below in a clean room; I relied on
@@ -46,15 +51,21 @@ import sqlite3
 from sqlite3 import Error
 
 import time
+import requests
+import json
 
 # -----------------------
 # Statics / defaults
 # -----------------------
 sqlite_db_file = './temperature.sqlite'
-# API information - https://www.weather.gov/documentation/services-web-api
-temperature_source_api = https://api.weather.gov
-temperature_source_key = 'rake74@gmail.com_for_NWEA_code_challenge'
+# API information - https://openweathermap.org/current
+temperature_source_api = "https://api.openweathermap.org/data/2.5/weather"
+temperature_source_key = sys.argv[0]
+location='Portland,OR,USA'
 
+# -----------------------
+# functions
+# -----------------------
 def create_sqlite_connection(db_file):
     # http://www.sqlitetutorial.net/sqlite-python/create-tables/
     """ create a database connection to a SQLite database """
@@ -79,14 +90,38 @@ def create_sqlite_table(conn, create_sqlite_table_sql):
     except Error as e:
         print(e)
 
+def save_temperature_to_sqlite()
+    #TODO
+
+def get_temperature_from_sqlite()
+    #TODO
+    now = int(time.time())
+    
+def get_temperature_from_source()
+    # inspiration:
+    # https://www.geeksforgeeks.org/python-find-current-weather-of-any-city-using-openweathermap-api/
+    source_query = "?appid=" + temperature_source_key + "&q=" + location
+    source_url = temperature_source_api + source_query
+    #TODO error handle below - including checking for non '200' response
+    source_response = requests.get(source_url)
+    current_kelvin = source_response.json()["main"]["temp"]
+    return current_kelvin
+
 def get_temperature()
     # TODO
     # query DB for most recent temp
     # if greater then 5 minutes ago, obtain from source API, and save to db
 
+
 def main()
     #TODO
     # start_listener here
+
+    # iterative deving, not listening, just call from command line for now
+    current_temperature = get_temperature()
+    print (current_temperature)
+
+    
 
 if __name__ == "__main__":
     main()
